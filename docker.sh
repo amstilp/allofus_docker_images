c8a0bf0ac147abaaf4ac72a22ee0725462b867c4
Adrienne Stilp <amstilp@uw.edu>
2025-02-24 16:03:45 -0800

    Add script to build and optionally push a docker image

---
+++ docker.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Script to build and (optionally) push a docker image, tagged with the current git sha.
+
+OPTSTRING=":p"
+
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    p)
+      push=true
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+# If pushing, check if the working directory is clean
+if [ "$push" = true ]; then
+  if [ -z "$(git status --porcelain)" ]; then
+    # Working directory clean
+    echo "clean"
+  else
+    # Uncommitted changes
+    git status
+    echo ""
+    echo "Please stash or commit changes before building and pushing the docker image."
+    exit 1
+  fi
+fi
+
+# Shift arguments so we can get the name of the image to push
+shift $(($OPTIND - 1))
+
+# Get the commit sha
+commit_hash=`git rev-parse --short HEAD`
+
+# Save the image
+image_name_and_tag="amstilp/aou_$1:$commit_hash"
+
+#docker build --platform linux/amd64 -t $image_name_and_tag $1
+
+echo "Built $image_name_and_tag"
+
+if [ "$push" = true ]; then
+  #docker push $image_name_and_tag
+  echo "Pushed $image_name_and_tag"
+fi
